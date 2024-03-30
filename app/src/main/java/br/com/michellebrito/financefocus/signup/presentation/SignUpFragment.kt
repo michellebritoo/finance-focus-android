b12af01ed694c7c4b2ac00a653eeba4774fdc2c3
michellebritoo <dev.michellebrito@gmail.com>
2024-03-30 19:22:04 -0300

    feat: create signup fragment and add into nav graph

---
+++ app/src/main/java/br/com/michellebrito/financefocus/signup/presentation/SignUpFragment.kt
@@ -0,0 +1,8 @@
+package br.com.michellebrito.financefocus.signup.presentation
+
+import androidx.fragment.app.Fragment
+import br.com.michellebrito.financefocus.R
+
+class SignUpFragment : Fragment(R.layout.fragment_sign_up) {
+
+}
