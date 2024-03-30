e692b9fc185a5ce63470460cf6017f98e3561adb
michellebritoo <dev.michellebrito@gmail.com>
2024-03-30 19:21:10 -0300

    feat: add koin DI dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,15 +39,17 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("io.insert-koin:koin-core:2.2.3")
+    implementation("io.insert-koin:koin-android:2.2.3")
 
     testImplementation("junit:junit:4.13.2")
 
