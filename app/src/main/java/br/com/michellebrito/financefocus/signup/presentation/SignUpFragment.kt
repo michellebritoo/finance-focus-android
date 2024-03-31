28b3341217a7f105d8dff327e204b9e8b147d939
michellebritoo <dev.michellebrito@gmail.com>
2024-03-31 16:01:00 -0300

    feat: add signup email step

--- app/src/main/java/br/com/michellebrito/financefocus/signup/presentation/SignUpFragment.kt
+++
@@ -1,8 +0,0 @@
-package br.com.michellebrito.financefocus.signup.presentation
-
-import androidx.fragment.app.Fragment
-import br.com.michellebrito.financefocus.R
-
-class SignUpFragment : Fragment(R.layout.fragment_sign_up) {
-
-}
---
+++ app/src/main/java/br/com/michellebrito/financefocus/signup/presentation/email/SignUpEmailStepFragment.kt
@@ -0,0 +1,8 @@
+package br.com.michellebrito.financefocus.signup.presentation.email
+
+import androidx.fragment.app.Fragment
+import br.com.michellebrito.financefocus.R
+
+class SignUpEmailStepFragment : Fragment(R.layout.fragment_sign_up) {
+
+}
