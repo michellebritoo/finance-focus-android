5519f2750eb4a659977659e7c758c9d3af73d64f
michellebritoo <dev.michellebrito@gmail.com>
2024-03-31 16:17:54 -0300

    feat: add signup password step

--- app/src/main/java/br/com/michellebrito/financefocus/signup/presentation/email/SignUpEmailStepFragment.kt
+++
@@ -1,8 +0,0 @@
-package br.com.michellebrito.financefocus.signup.presentation.email
-
-import androidx.fragment.app.Fragment
-import br.com.michellebrito.financefocus.R
-
-class SignUpEmailStepFragment : Fragment(R.layout.fragment_sign_up) {
-
-}
