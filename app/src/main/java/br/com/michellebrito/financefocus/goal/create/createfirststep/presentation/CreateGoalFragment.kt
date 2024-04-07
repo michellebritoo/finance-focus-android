9d7578f3be0d3d5bb05892242a094fc8d2f6a4b2
michellebritoo <dev.michellebrito@gmail.com>
2024-04-07 18:44:37 -0300

    feat: add UI rules

--- app/src/main/java/br/com/michellebrito/financefocus/goal/create/createfirststep/presentation/CreateGoalFragment.kt
+++ app/src/main/java/br/com/michellebrito/financefocus/goal/create/createfirststep/presentation/CreateGoalFragment.kt
@@ -1,5 +1,6 @@
 package br.com.michellebrito.financefocus.goal.create.createfirststep.presentation
 
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import br.com.michellebrito.financefocus.R
@@ -8,15 +9,24 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 
 class CreateGoalFragment : Fragment(R.layout.fragment_create_goal) {
     private val binding: FragmentCreateGoalBinding by viewBinding()
-
+    private val viewModel = CreateGoalViewModel()
     override fun onResume() {
         super.onResume()
         setupListeners()
+        subscribeViewModelEvents()
     }
 
     private fun setupListeners() = with(binding) {
         topBar.setNavigationOnClickListener {
             findNavController().popBackStack()
+        }
+        ETTitle.doAfterTextChanged { viewModel.onTitleChanged(it.toString()) }
+        ETValue.doAfterTextChanged { viewModel.onValueChanged(it.toString()) }
+    }
+
+    private fun subscribeViewModelEvents() {
+        viewModel.enableButton.observe(this) { result ->
+            binding.btnContinue.isEnabled = result
         }
     }
 }
