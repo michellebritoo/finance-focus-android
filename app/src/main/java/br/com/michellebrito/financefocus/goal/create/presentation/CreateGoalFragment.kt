f420fcef6098a0b58c1aded01ff0e20a0813d939
michellebritoo <dev.michellebrito@gmail.com>
2024-04-07 11:25:44 -0300

    feat: create goal fragment

---
+++ app/src/main/java/br/com/michellebrito/financefocus/goal/create/presentation/CreateGoalFragment.kt
@@ -0,0 +1,8 @@
+package br.com.michellebrito.financefocus.goal.create.presentation
+
+import androidx.fragment.app.Fragment
+import br.com.michellebrito.financefocus.R
+
+class CreateGoalFragment : Fragment(R.layout.fragment_create_goal) {
+
+}
