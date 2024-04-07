9fdb2f2babc09804d149a9b9a1be5a2d611c86a9
michellebritoo <dev.michellebrito@gmail.com>
2024-04-07 12:01:39 -0300

    feat: include create goal screen into nav graph

--- app/src/main/java/br/com/michellebrito/financefocus/goal/create/presentation/CreateGoalFragment.kt
+++ app/src/main/java/br/com/michellebrito/financefocus/goal/create/presentation/CreateGoalFragment.kt
@@ -1,8 +1,22 @@
 package br.com.michellebrito.financefocus.goal.create.presentation
 
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import br.com.michellebrito.financefocus.R
+import br.com.michellebrito.financefocus.databinding.FragmentCreateGoalBinding
+import by.kirich1409.viewbindingdelegate.viewBinding
 
 class CreateGoalFragment : Fragment(R.layout.fragment_create_goal) {
+    private val binding: FragmentCreateGoalBinding by viewBinding()
 
+    override fun onResume() {
+        super.onResume()
+        setupListeners()
+    }
+
+    private fun setupListeners() = with(binding) {
+        appBar.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
 }
