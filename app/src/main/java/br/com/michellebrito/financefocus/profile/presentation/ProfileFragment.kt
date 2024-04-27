8618d95a498cfcbc4f97c719fafdf9dab40d48a5
michellebritoo <dev.michellebrito@gmail.com>
2024-04-27 19:47:21 -0300

    feat: create profile details fragment

---
+++ app/src/main/java/br/com/michellebrito/financefocus/profile/presentation/ProfileFragment.kt
@@ -0,0 +1,7 @@
+package br.com.michellebrito.financefocus.profile.presentation
+
+import androidx.fragment.app.Fragment
+import br.com.michellebrito.financefocus.R
+
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+}
