65159bc7aaa9b780e5170fe9f7df3f65d1d1e2b7
michellebritoo <dev.michellebrito@gmail.com>
2024-05-11 16:45:52 -0300

    feat: add rates result fragment and add into nav graph

---
+++ app/src/main/java/br/com/michellebrito/financefocus/rates/result/CalculateRatesResultFragment.kt
@@ -0,0 +1,11 @@
+package br.com.michellebrito.financefocus.rates.result
+
+import androidx.fragment.app.Fragment
+import br.com.michellebrito.financefocus.R
+import br.com.michellebrito.financefocus.databinding.FragmentCalculateRatesResultBinding
+import by.kirich1409.viewbindingdelegate.viewBinding
+
+class CalculateRatesResultFragment : Fragment(R.layout.fragment_calculate_rates_result) {
+    private val binding: FragmentCalculateRatesResultBinding by viewBinding()
+
+}
