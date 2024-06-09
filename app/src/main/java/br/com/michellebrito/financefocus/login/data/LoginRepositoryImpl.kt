532a5e366155076c1df45c3020a4a6fe18356b97
michellebritoo <dev.michellebrito@gmail.com>
2024-06-09 11:46:19 -0300

    feat: store user session infos into storage

--- app/src/main/java/br/com/michellebrito/financefocus/login/data/LoginRepositoryImpl.kt
+++ app/src/main/java/br/com/michellebrito/financefocus/login/data/LoginRepositoryImpl.kt
@@ -1,11 +1,14 @@
 package br.com.michellebrito.financefocus.login.data
 
+import br.com.michellebrito.financefocus.common.domain.PreferenceStorage
 import br.com.michellebrito.financefocus.login.domain.LoginRepository
+import com.google.android.gms.tasks.Task
 import com.google.firebase.Firebase
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
 
-class LoginRepositoryImpl : LoginRepository {
+class LoginRepositoryImpl(private val preferenceStorage: PreferenceStorage) : LoginRepository {
     private var auth: FirebaseAuth = Firebase.auth
 
     override fun authWithEmailAndPassword(
@@ -15,7 +18,23 @@ class LoginRepositoryImpl : LoginRepository {
     ) {
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    storeLoggedInfoIntoStorage(task)
+                }
                 callback(task.isSuccessful)
             }
+    }
+
+    private fun storeLoggedInfoIntoStorage(task: Task<AuthResult>) {
+        preferenceStorage.putBoolean(
+            IS_LOGGED.plus(task.result.user?.uid),
+            task.isSuccessful
+        )
+        task.result.user?.let { preferenceStorage.putString(CURRENT_USER, it.uid) }
+    }
+
+    private companion object {
+        const val IS_LOGGED = "is_logged"
+        const val CURRENT_USER = "current_user"
     }
 }
