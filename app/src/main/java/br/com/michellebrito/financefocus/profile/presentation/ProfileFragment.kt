2a2b57146b4757702408f279f1be4c944ffb0a48
michellebritoo <dev.michellebrito@gmail.com>
2024-07-07 16:51:27 -0300

    feature: observe UI state

--- app/src/main/java/br/com/michellebrito/financefocus/profile/presentation/ProfileFragment.kt
+++ app/src/main/java/br/com/michellebrito/financefocus/profile/presentation/ProfileFragment.kt
@@ -2,15 +2,21 @@ package br.com.michellebrito.financefocus.profile.presentation
 
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import br.com.michellebrito.financefocus.MainActivity
 import br.com.michellebrito.financefocus.R
 import br.com.michellebrito.financefocus.databinding.FragmentProfileBinding
 import by.kirich1409.viewbindingdelegate.viewBinding
+import org.koin.android.ext.android.inject
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
     private val binding: FragmentProfileBinding by viewBinding()
+    private val viewModel: ProfileFragmentViewModel by inject()
+
     override fun onResume() {
         super.onResume()
         setupListeners()
+        setupObservers()
+        viewModel.getUserDetails()
     }
 
     private fun setupListeners() = with(binding) {
@@ -23,6 +29,40 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
                 R.id.item_goals -> findNavController().navigate(R.id.listGoalFragment)
             }
             true
+        }
+    }
+
+    private fun setupObservers() {
+        viewModel.viewState.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                is ProfileEvent.ShowLoading -> showLoading()
+                is ProfileEvent.HideLoading -> hideLoading()
+                is ProfileEvent.ShowError -> showError()
+                is ProfileEvent.ShowUserInfo -> showUserInfo(
+                    state.name,
+                    state.email,
+                    state.goals,
+                    state.rates
+                )
+            }
+        }
+    }
+
+    private fun showLoading() = (requireActivity() as MainActivity).showLoading()
+
+    private fun hideLoading() = (requireActivity() as MainActivity).hideLoading()
+
+    private fun showError() {
+        //task 35
+    }
+
+    private fun showUserInfo(name: String, email: String, goals: String, rates: String) {
+        with(binding) {
+            tvSalutation.text = getString(R.string.profile_salutation, name)
+            tdEmail.setText(getString(R.string.profile_email, email))
+            tdNotifications.setText(getString(R.string.profile_notification))
+            tdGoals.setText(getString(R.string.profile_goals, goals))
+            tdRates.setText(getString(R.string.profile_rates, rates))
         }
     }
 }
