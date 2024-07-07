ef7d7c175649ed2997d96fec8c7b6e28c910d299
michellebritoo <dev.michellebrito@gmail.com>
2024-07-07 14:41:38 -0300

    feature: store authorization and refresh device token

--- app/src/main/java/br/com/michellebrito/financefocus/welcome/data/WelcomeRepositoryImpl.kt
+++ app/src/main/java/br/com/michellebrito/financefocus/welcome/data/WelcomeRepositoryImpl.kt
@@ -3,9 +3,25 @@ package br.com.michellebrito.financefocus.welcome.data
 import br.com.michellebrito.financefocus.common.domain.PreferenceStorage
 import br.com.michellebrito.financefocus.welcome.domain.WelcomeRepository
 
-class WelcomeRepositoryImpl(private val preferenceStorage: PreferenceStorage): WelcomeRepository {
+class WelcomeRepositoryImpl(
+    private val preferenceStorage: PreferenceStorage,
+    private val client: WelcomeClient
+): WelcomeRepository {
+
     override fun isLoggedUser(): Boolean {
         return preferenceStorage.getBoolean(IS_LOGGED.plus(getCurrentUser()))
+    }
+
+    override fun setAuthorizationToken(authorization: String) {
+        preferenceStorage.putString(AUTHORIZATION, authorization)
+    }
+
+    override fun getAuthorizationToken(): String {
+        return preferenceStorage.getString(AUTHORIZATION)
+    }
+
+    override suspend fun sendDeviceToken(token: String) {
+        client.sendDeviceToken(getAuthorizationToken(), token)
     }
 
     private fun getCurrentUser() = preferenceStorage.getString(CURRENT_USER)
@@ -13,5 +29,6 @@ class WelcomeRepositoryImpl(private val preferenceStorage: PreferenceStorage): W
     private companion object {
         const val IS_LOGGED = "is_logged"
         const val CURRENT_USER = "current_user"
+        const val AUTHORIZATION = "authorization"
     }
 }
