2c32077db15e6eab02de363b5c7224ed42aff184
michellebritoo <dev.michellebrito@gmail.com>
2024-07-07 14:39:25 -0300

    feature: send device token to backend

---
+++ app/src/main/java/br/com/michellebrito/financefocus/welcome/data/WelcomeClient.kt
@@ -0,0 +1,19 @@
+package br.com.michellebrito.financefocus.welcome.data
+
+import retrofit2.http.Header
+import retrofit2.http.POST
+
+interface WelcomeClient {
+
+    @POST(REFRESH_DEVICE_TOKEN)
+    suspend fun sendDeviceToken(
+        @Header(HEADER_AUTHORIZATION) authorization: String,
+        @Header(HEADER_DEVICE_TOKEN) token: String
+    )
+
+    private companion object {
+        const val REFRESH_DEVICE_TOKEN = "/refresh/device/token"
+        const val HEADER_AUTHORIZATION = "authorization"
+        const val HEADER_DEVICE_TOKEN = "deviceToken"
+    }
+}
