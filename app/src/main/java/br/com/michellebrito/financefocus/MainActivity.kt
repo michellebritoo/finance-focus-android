378f5da4b5711c786e787a3479a34ca70b9288fb
michellebritoo <dev.michellebrito@gmail.com>
2024-07-07 14:40:38 -0300

    feature: get device token from FCM

--- app/src/main/java/br/com/michellebrito/financefocus/MainActivity.kt
+++ app/src/main/java/br/com/michellebrito/financefocus/MainActivity.kt
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import br.com.michellebrito.financefocus.databinding.ActivityMainBinding
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by viewBinding()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,5 +27,14 @@ class MainActivity : AppCompatActivity() {
             isVisible = false
             isIndeterminate = false
         }
+    }
+
+    fun getDeviceToken(callback: (String) -> Unit) {
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    callback(task.result.toString())
+                }
+            }
     }
 }
