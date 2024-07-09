fcb7528705e7055cf016e48c4291fd1991e804be
michellebritoo <dev.michellebrito@gmail.com>
2024-07-09 17:50:46 -0300

    feature: show list from endpoint

--- app/src/main/java/br/com/michellebrito/financefocus/goal/list/presentation/ListGoalFragment.kt
+++ app/src/main/java/br/com/michellebrito/financefocus/goal/list/presentation/ListGoalFragment.kt
@@ -3,18 +3,23 @@ package br.com.michellebrito.financefocus.goal.list.presentation
 import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import br.com.michellebrito.financefocus.MainActivity
 import br.com.michellebrito.financefocus.R
 import br.com.michellebrito.financefocus.databinding.FragmentListGoalBinding
 import br.com.michellebrito.financefocus.goal.list.model.ListGoalItemModel
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.snackbar.Snackbar
+import org.koin.android.ext.android.inject
 
 class ListGoalFragment : Fragment(R.layout.fragment_list_goal) {
     private val binding: FragmentListGoalBinding by viewBinding()
+    private val viewModel: ListGoalViewModel by inject()
 
     override fun onResume() {
         super.onResume()
-        setupView()
         setupListeners()
+        observeEvents()
+        viewModel.getGoalsList()
     }
 
     private fun setupListeners() = with(binding) {
@@ -36,20 +41,29 @@ class ListGoalFragment : Fragment(R.layout.fragment_list_goal) {
         }
     }
 
-    private fun setupView() = with(binding) {
-        bottomNavigation.selectedItemId = R.id.item_goals
-        rvGoalList.adapter = ListGoalAdapter(
-            listOf(
-                ListGoalItemModel("TV", "20/07/2022"),
-                ListGoalItemModel("Celular", "20/10/2022"),
-                ListGoalItemModel("Celular", "20/10/2022"),
-                ListGoalItemModel("Celular", "20/10/2022"),
-                ListGoalItemModel("Celular", "20/10/2022"),
-                ListGoalItemModel("Celular", "20/10/2022"),
-                ListGoalItemModel("Celular", "20/10/2022"),
-                ListGoalItemModel("Celular", "20/10/2022"),
-            )
-        ).apply {
+    private fun observeEvents() {
+        viewModel.viewState.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                is ListGoalEvent.ShowLoading -> showLoading()
+                is ListGoalEvent.HideLoading -> hideLoading()
+                is ListGoalEvent.ShowList -> showList(state.list)
+                is ListGoalEvent.ShowError -> showError()
+            }
+        }
+    }
+
+    private fun showLoading() = (requireActivity() as MainActivity).showLoading()
+
+    private fun hideLoading() = (requireActivity() as MainActivity).hideLoading()
+
+    private fun showError() {
+        this@ListGoalFragment.view?.let {
+            Snackbar.make(it, R.string.cannot_proceed_generic_error, Snackbar.LENGTH_LONG).show()
+        }
+    }
+
+    private fun showList(list: List<ListGoalItemModel>) = with(binding) {
+        rvGoalList.adapter = ListGoalAdapter(list).apply {
             onItemClick = {
                 findNavController().navigate(R.id.listGoalFragmentToGoalDetailsFragment)
             }
