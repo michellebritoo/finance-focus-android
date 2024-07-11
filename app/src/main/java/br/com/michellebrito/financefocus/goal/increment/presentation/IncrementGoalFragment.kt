1e7eb387789de5bca3807a8d30fb954daaca2b16
michellebritoo <dev.michellebrito@gmail.com>
2024-07-11 09:39:48 -0300

    feature: create base to increment goal screen

--- app/src/main/java/br/com/michellebrito/financefocus/goal/increment/presentation/IncrementGoalFragment.kt
+++ app/src/main/java/br/com/michellebrito/financefocus/goal/increment/presentation/IncrementGoalFragment.kt
@@ -1,6 +1,7 @@
 package br.com.michellebrito.financefocus.goal.increment.presentation
 
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import br.com.michellebrito.financefocus.R
 import br.com.michellebrito.financefocus.databinding.FragmentIncrementGoalBinding
 import by.kirich1409.viewbindingdelegate.viewBinding
@@ -17,7 +18,10 @@ class IncrementGoalFragment : Fragment(R.layout.fragment_increment_goal) {
         args?.let {
             viewModel.onStart(args.goaltoincrement)
         }
+        setupListeners()
     }
 
-
+    private fun setupListeners() = with(binding) {
+        topBar.setNavigationOnClickListener { findNavController().popBackStack() }
+    }
 }
